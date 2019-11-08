c5824dc0bce1f9b8c757106b74b1aa0c3c1c68fb
avi <avikan1087@gmail.com>
2019-11-08 14:08:54 -0500

    finish mvp Wars.

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -20,7 +20,6 @@ const CharacterCard = (props) => {
     <h1>{props.data.name}</h1>
     <p><b>DOB:</b> {props.data.birth_year}</p>
     <p><b>Height:</b> {props.data.height}</p>
-    <p><b>Weight:</b> {props.data.height}</p>
     <p><b>Gender:</b> {props.data.gender}</p>
     <p><b>Hair Color:</b> {props.data.hair_color}</p>
     <p><b>Skin Color:</b> {props.data.skin_color}</p>
